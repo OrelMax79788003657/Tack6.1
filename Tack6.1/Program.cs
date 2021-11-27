d948e619bb1c73b1759263776e566f0f8cf43ebd
USER <USER@DESKTOP-FRBRUEK>
2021-11-27 21:44:36 +0300

    Добавьте файлы проекта.

---
+++ Tack6.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tack6._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку(знаки препинания не используются): ");
+            string startString = Console.ReadLine();
+            string[] stringArray = startString.Split();
+            int maxlen =0, index = 0;
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                if (stringArray[i].Length > maxlen)
+                {
+                    maxlen = stringArray[i].Length;
+                    index = i;
+                }
+            }
+            Console.Write("Самое длинное слово: {0}", stringArray[index]);
+            Console.ReadKey();
+        }
+    }
+}
